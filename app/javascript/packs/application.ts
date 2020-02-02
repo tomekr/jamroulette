798f460b8fa13b6de46bbafc783258e870eaf275
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-02-02 14:05:31 -0600

    Import bulma and use typescript for application.ts file

---
+++ app/javascript/packs/application.ts
@@ -0,0 +1,9 @@
+import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import "controllers"
