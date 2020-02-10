71c81b74454172691009d7f5cfd97d512dded57f
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-02-10 16:26:17 -0600

    Use webpack based bulma instead of CDN

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,7 +1,13 @@
 import * as ActiveStorage from "@rails/activestorage"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "styles/styles"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// This is so that we can reference images in app/javascript/packs/images
+const images = require.context("../images", true)
+// @ts-ignore
+const imagePath = name => images(name, true)
