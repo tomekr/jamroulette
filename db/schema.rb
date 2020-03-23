ad1c28c0d263ba0fd294bc05503ed2beadb702f4
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-23 10:47:15 -0500

    Add Active Storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_182816) do
+ActiveRecord::Schema.define(version: 2020_03_23_154645) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "jams", force: :cascade do |t|
     t.string "filename"
@@ -29,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_03_22_182816) do
     t.index ["room_hash"], name: "index_rooms_on_room_hash", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "jams", "rooms"
 end
