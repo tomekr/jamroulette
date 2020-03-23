c4fe59cf4b1377bdedb8867f1b36ec8e91f4a7bc
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-23 11:38:27 -0500

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+room = Room.create
+room.room_hash = 'jam'
+room.save
+
+jam = room.jams.build(bpm: '120')
+jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3')
+jam.save
+
+other_jam = room.jams.build(bpm: '120')
+other_jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3')
+other_jam.save
+
+room_without_jam = Room.create
+room_without_jam.room_hash = 'nojam'
+room_without_jam.save
