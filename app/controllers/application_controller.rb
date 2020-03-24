d2b3ef2284b3329d50ef7bc61b88b2bf355334d5
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-24 16:13:58 -0500

    Check for beta code on every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :ensure_beta_user
+
+  private
+
+  def ensure_beta_user
+    redirect_to root_path unless session[:is_beta_user]
+  end
 end
