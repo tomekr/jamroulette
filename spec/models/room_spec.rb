138119bc8625842a909301b398e18a6305ec4a58
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-24 12:11:04 -0500

    Add room factory validation spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe 'Validations' do
+    let(:room) { build(:room) }
+
+    it 'is valid out of the factory' do
+      expect(room).to be_valid
+    end
+  end
+
   it 'generates a random hex room hash' do
     room = create(:room)
     expect(room.room_hash).to match(/[0-9a-f]{32}/)
