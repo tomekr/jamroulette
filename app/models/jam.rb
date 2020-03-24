bf74f9dcf67a490b051127d116bdcfe4a4d6feb0
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-24 10:59:02 -0500

    Add filename extraction to Jam model

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -4,4 +4,12 @@ class Jam < ApplicationRecord
   belongs_to :room
   has_one_attached :file
   validates :file, presence: true
+
+  before_create :extract_filename
+
+  private
+
+  def extract_filename
+    self.filename = self.file.blob.filename
+  end
 end
--- spec/factories/jam_factory.rb
+++ spec/factories/jam_factory.rb
@@ -2,7 +2,6 @@
 
 FactoryBot.define do
   factory :jam do
-    filename { 'the-darkest-of-toasters.wav' }
     bpm { '120' }
     file { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test.mp3'), 'audio/mpeg') }
     room
