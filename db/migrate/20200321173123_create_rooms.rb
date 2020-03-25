6cbe2fc41ba5abd1e43c5a81b8ebb40f818d5864
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-25 14:56:32 -0500

    Make room_hash a case-insensitive column

--- db/migrate/20200321173123_create_rooms.rb
+++ db/migrate/20200321173123_create_rooms.rb
@@ -4,7 +4,7 @@ class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
       t.string :title
-      t.string :room_hash, null: false, index: { unique: true }
+      t.citext :room_hash, null: false, index: { unique: true }
 
       t.timestamps
     end
