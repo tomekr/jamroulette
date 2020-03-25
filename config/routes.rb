823ceeeecd5c384b7fdf2b0c18421d1bcea0099d
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-25 13:29:58 -0500

    Add beta invite code action, view, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'static#index'
+  root 'static#beta'
+  post '/validate_beta_user', to: 'static#validate_beta_user'
+  get 'home', to: 'static#index'
 
   resources :rooms, only: %i[create show] do
     resources :jams, only: :create
