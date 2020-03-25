271b8cabb0f1d8e25185adf9206fbcf762b1a3f1
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-25 15:01:16 -0500

    Add postgres config for Travis CI

--- db/migrate/20200321173123_create_rooms.rb
+++ db/migrate/20200321173123_create_rooms.rb
@@ -2,6 +2,8 @@
 
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
+    enable_extension 'citext'
+
     create_table :rooms do |t|
       t.string :title
       t.citext :room_hash, null: false, index: { unique: true }
