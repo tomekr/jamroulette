279b0b7e548cf2182c6feeef66d52c8c456f57fc
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-27 11:55:10 -0500

    Remove unused random_room route (#19)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'static#beta'
   post '/validate_beta_user', to: 'static#validate_beta_user'
   get 'home', to: 'static#index'
-  get 'rooms/random_room', to: 'rooms#random_room', as: 'random_room'
 
   resources :rooms, only: %i[create show] do
     resources :jams, only: :create
