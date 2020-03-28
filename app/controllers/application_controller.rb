445c6f4ddc93003997521fb4b90c8f8496c2300e
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-28 16:15:22 -0500

    Add display_name to sign_up and account_update permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :ensure_beta_user
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:display_name])
+  end
 
   def ensure_beta_user
     unless session[:is_beta_user]
