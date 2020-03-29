ecdb82027ca20e890944971105ce19e776e1b235
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-29 18:18:38 -0500

    Add an ActivityPresenter

---
+++ app/presenters/activity_presenter.rb
@@ -0,0 +1,30 @@
+class ActivityPresenter < SimpleDelegator
+  include ActionView::Helpers::SanitizeHelper
+
+  def self.activities(*activities)
+    activities.flatten.map { |activity| ActivityPresenter.new(activity) }
+  end
+
+  def initialize(activity)
+    @activity = activity
+    super
+  end
+
+  def activity_feed_line
+    case @activity.subject_type
+    when "Jam"
+      jam = @activity.subject
+      filename = sanitize(jam.file.filename.to_s)
+      "<span class='jam-activity'>You uploaded #{filename} to #{room_link_for(jam.room)}</span>".html_safe
+    when "Room"
+      room = @activity.subject
+      "<span class='jam-activity'>You created #{room_link_for(room)}</span>".html_safe
+    end
+  end
+
+  private
+
+  def room_link_for(room)
+    "<a href='/rooms/#{room.public_id}'>#{room.public_id}"
+  end
+end
---
+++ spec/presenters/activity_presenter_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe ActivityPresenter do
+  # instance_double won't work for ActiveStorage::Attached::One since filename
+  # is not an instance_method on that class.
+  let(:file) { double(ActiveStorage::Attached::One, filename: 'test.mp3') }
+  let(:room) { instance_double(Room, public_id: 'activity-presenter-room') }
+  let(:jam) { instance_double(Jam, file: file, room: room) }
+
+  let(:room_activity) { instance_double(Activity, subject: room) }
+  let(:jam_activity) { instance_double(Activity, subject: jam) }
+
+  let(:presenter) { ActivityPresenter.new(activity) }
+
+  context 'presenting a jam' do
+    let(:activity) { jam_activity }
+
+    it "displays filename and room link" do
+      allow(jam_activity).to receive(:subject_type).and_return("Jam")
+      expect(presenter.activity_feed_line).to eq(
+        "<span class='jam-activity'>You uploaded test.mp3 to <a href='/rooms/activity-presenter-room'>activity-presenter-room</span>"
+      )
+    end
+  end
+
+  context 'presenting a room' do
+    let(:activity) { room_activity }
+
+    it "displays the room public_id" do
+      allow(room_activity).to receive(:subject_type).and_return("Room")
+      expect(presenter.activity_feed_line).to eq(
+        "<span class='jam-activity'>You created <a href='/rooms/activity-presenter-room'>activity-presenter-room</span>"
+      )
+    end
+  end
+end
