f2fc79a0fc89bbfc2a401693b8529dbfb98644df
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-29 09:14:53 -0500

    fixup: allow unauthenticated user to click create room but redirect to sign in

--- spec/requests/home_page_request_spec.rb
+++ spec/requests/home_page_request_spec.rb
@@ -4,14 +4,29 @@ require 'rails_helper'
 
 RSpec.describe 'Home page viewing', type: :request do
   # TODO: Remove when beta invite requirements are removed
-  it 'redirects to room after code validation if user first visited room' do
+  def validate_beta_user
     InviteCode.create(code: 'correct-code')
+    post '/validate_beta_user', params: { beta_code: 'correct-code' }
+  end
+
+  it 'redirects to room after code validation if user first visited room' do
     room = create(:room)
 
     get room_path(room)
     follow_redirect!
 
-    post '/validate_beta_user', params: { beta_code: 'correct-code' }
+    validate_beta_user
     expect(response).to redirect_to(room_path(room))
+  end
+
+  context "user is unauthenticated" do
+    before(:each) { validate_beta_user }
+
+    context "creating a room" do
+      it 'redirects to sign in page' do
+        post rooms_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 end
