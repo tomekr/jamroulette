d597b6b935f7228fa83dfb2aa204ff0a1fc34da2
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-29 08:39:16 -0500

    Fix Heroku Review Apps active storage config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  # Store uploaded files on the local file system for Heroku Review Apps
+  # otherwise use Amazon S3
+  if ENV["HEROKU_REVIEW_APP"]
+    config.active_storage.service = :local
+  else
+    config.active_storage.service = :amazon
+  end
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
