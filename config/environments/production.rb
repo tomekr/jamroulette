6688dbf9c116adb747089e1082958446b97085d0
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-30 08:43:19 -0500

    fixup: set domain for smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'jamroulette.app',
     :enable_starttls_auto => true
   }
 
