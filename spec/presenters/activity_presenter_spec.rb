811affc5a6f04552fdaf4e2b77c3994240f7a4e8
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-03-30 09:47:38 -0500

    fixup: use factories for ActivityPresenter spec

--- spec/presenters/activity_presenter_spec.rb
+++ spec/presenters/activity_presenter_spec.rb
@@ -1,22 +1,15 @@
 require "rails_helper"
 
 describe ActivityPresenter do
-  # instance_double won't work for ActiveStorage::Attached::One since filename
-  # is not an instance_method on that class.
-  let(:file) { double(ActiveStorage::Attached::One, filename: 'test.mp3') }
-  let(:room) { instance_double(Room, public_id: 'activity-presenter-room') }
-  let(:jam) { instance_double(Jam, file: file, room: room) }
-
-  let(:room_activity) { instance_double(Activity, subject: room) }
-  let(:jam_activity) { instance_double(Activity, subject: jam) }
-
+  let(:room) { build(:room, public_id: 'activity-presenter-room') }
   let(:presenter) { ActivityPresenter.new(activity) }
 
   context 'presenting a jam' do
-    let(:activity) { jam_activity }
+    let(:jam) { build(:jam, room: room) }
+    let(:activity) { create(:activity, :room, subject: jam) }
 
     it "displays filename and room link" do
-      allow(jam_activity).to receive(:subject_type).and_return("Jam")
+      allow(activity).to receive(:subject_type).and_return("Jam")
       expect(presenter.activity_feed_line).to eq(
         "<span class='jam-activity'>You uploaded test.mp3 to <a href=\"/rooms/activity-presenter-room\">activity-presenter-room</a></span>"
       )
@@ -24,10 +17,10 @@ describe ActivityPresenter do
   end
 
   context 'presenting a room' do
-    let(:activity) { room_activity }
+    let(:activity) { create(:activity, :room, subject: room) }
 
     it "displays the room link" do
-      allow(room_activity).to receive(:subject_type).and_return("Room")
+      allow(activity).to receive(:subject_type).and_return("Room")
       expect(presenter.activity_feed_line).to eq(
         "<span class='room-activity'>You created <a href=\"/rooms/activity-presenter-room\">activity-presenter-room</a></span>"
       )
