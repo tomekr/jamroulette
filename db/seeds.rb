ee989942102d136ee9ef86e33688595dc53a7878
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-04-03 19:01:04 -0500

    Activites no longer need to be explicitly created with after_create call

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,21 +26,17 @@ alice = User.create(
 
 # Create an empty room
 empty_room = Room.create!(public_id: 'empty', user: bob, name: 'Empty Room')
-empty_room.activities.create!(user: bob)
 
 # Create a room with two attached jams
 room = Room.create!(public_id: 'jams', user: bob, name: 'Jam Room')
-room.activities.create!(user: bob)
 
 jam = room.jams.build(bpm: '120', user: bob)
 jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3', content_type: 'audio/mpeg')
 jam.save
-jam.activities.create(user: jam.user)
 
 other_jam = room.jams.build(bpm: '120', user: alice)
 other_jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3', content_type: 'audio/mpeg')
 other_jam.save
-other_jam.activities.create(user: other_jam.user)
 
 # TODO Remove when no longer in beta
 InviteCode.create(code: 'Mellon')
