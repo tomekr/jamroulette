ee29b911caa2c54507b42749786408ea549ede62
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-04-07 19:36:52 -0500

    Fix db seeds to support promotion

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,14 +30,13 @@ empty_room = Room.create!(public_id: 'empty', user: bob, name: 'Empty Room')
 # Create a room with two attached jams
 room = Room.create!(public_id: 'jams', user: bob, name: 'Jam Room')
 
-jam = room.jams.build(bpm_list: '120', user: bob)
+jam = room.jams.build(bpm_list: '120', jam_type_list: ['Mix'], promoted_at: Time.current, user: bob)
 jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3', content_type: 'audio/mpeg')
 jam.save
 
-other_jam = room.jams.build(bpm_list: '120', user: alice)
+other_jam = room.jams.build(bpm_list: '120', jam_type_list: ['Mix'], promoted_at: Time.current, user: alice)
 other_jam.file.attach(io: File.open('spec/support/assets/test.mp3'), filename: 'test.mp3', content_type: 'audio/mpeg')
 other_jam.save
 
-# TODO Remove when no longer in beta
+# TODO: Remove when no longer in beta
 InviteCode.create(code: 'Mellon')
-
