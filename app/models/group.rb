e3449a6141c76925a4f9634278cb625a77f914b7
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-04-16 14:18:55 -0500

    fixup: remove legacy association code

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,9 +4,6 @@ class Group < ApplicationRecord
   groupify :group, default_members: :users
   validates :name, presence: true
 
-  # has_many :owners, through: :group_memberships, source: :user
-  # has_many :users, through: :group_memberships
-  # has_many :group_members, dependent: :destroy
   scope :discoverable, -> { where(visible: true) }
 
   def add_owner(user)
