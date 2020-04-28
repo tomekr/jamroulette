e8011d1e2f769242f19da17d8005458e4bef166f
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-04-28 10:40:24 -0500

    Use Active Storage Direct Upload to upload files directly to storage provider (#62)
    * Transition to Active Storage Direct Upload
    * Direct Upload with DirectUploadController and progress bar

--- app/javascript/controllers/jam_upload_controller.ts
+++ app/javascript/controllers/jam_upload_controller.ts
@@ -1,23 +1,48 @@
 import { Controller } from "stimulus"
+import { DirectUpload } from "@rails/activestorage"
 
 export default class extends Controller {
-  static classes = ["active"]
-  static targets = ["uploadModal", "uploadForm", "fileField", "fileName", "durationField", "bpmField"]
+  static classes = ["active", "progressLoading", "progressDone"]
+  static targets = ["uploadModal", "uploadForm", "fileField", "fileName", "durationField", "bpmField", "progressBar"]
 
+  // Classes
   activeClass: string
+  progressLoadingClass: string
+  progressDoneClass: string
+
+  // Targets
   uploadFormTarget: HTMLFormElement
   uploadModalTarget: HTMLElement
   fileFieldTarget: HTMLInputElement
   durationFieldTarget: HTMLInputElement
   fileNameTarget: HTMLSpanElement
   bpmFieldTarget: HTMLInputElement
+  progressBarTarget: HTMLProgressElement
 
   toggleModal(event: CustomEvent) {
-    event.preventDefault()
     this.uploadModalTarget.classList.toggle(this.activeClass)
   }
 
-  upload() {
+  uploadFile(file: File): void {
+    const url = this.fileFieldTarget.dataset.directUploadUrl
+    const upload = new DirectUpload(file, url)
+    upload.create((error, blob) => {
+      if (error) {
+        throw new Error('Direct Upload Error - ' + error)
+      } else {
+        // Add an appropriately-named hidden input to the form with a
+        // value of blob.signed_id so that the blob ids will be
+        // transmitted in the normal upload flow
+        const hiddenField = document.createElement('input')
+        hiddenField.setAttribute("type", "hidden")
+        hiddenField.setAttribute("value", blob.signed_id)
+        hiddenField.name = this.fileFieldTarget.name
+        this.uploadFormTarget.appendChild(hiddenField)
+      }
+    })
+  }
+
+  submitForm() {
     this.uploadFormTarget.submit()
   }
 
@@ -59,8 +84,83 @@ export default class extends Controller {
         this.extractDurationFromAudioFile(fileToUpload)
       }
 
+      const directUploadController = new DirectUploadController(this, fileToUpload, this.progressBarTarget)
+      directUploadController.start()
+
       // Send focus to bpm input
       this.bpmFieldTarget.focus()
+    }
+  }
+}
+
+class DirectUploadController {
+  directUpload: DirectUpload
+  source: Controller
+  file: File
+  xhr: XMLHttpRequest
+  hiddenInput: HTMLInputElement
+  progressBar: HTMLProgressElement
+
+  constructor(source: Controller, file: File, progressBar: HTMLProgressElement) {
+    this.directUpload = new DirectUpload(file, source.fileFieldTarget.dataset.directUploadUrl, this)
+    this.source = source
+    this.progressBar = progressBar
+    this.file = file
+  }
+
+  public start(): void {
+    this.hiddenInput = this.createHiddenInput()
+    this.uploadRequestStarted()
+    this.directUpload.create((error, blob) => {
+      if (error) {
+        this.removeElement(this.hiddenInput)
+      } else {
+        this.hiddenInput.value = blob.signed_id
+        this.hiddenInput.name = this.source.fileFieldTarget.name
+      }
+    })
+  }
+
+  public directUploadWillStoreFileWithXHR(xhr): void {
+    this.bindProgressEvent(xhr)
+  }
+
+  private bindProgressEvent(xhr: XMLHttpRequest): void {
+    this.xhr = xhr
+    this.xhr.upload.addEventListener("progress", event =>
+      this.uploadRequestDidProgress(event)
+    )
+
+    this.xhr.addEventListener("load", () => {
+      this.uploadRequestFinished()
+    })
+  }
+
+  private uploadRequestStarted(): void {
+    this.progressBar.value = 0
+    this.progressBar.classList.remove(this.source.progressDoneClass)
+    this.progressBar.classList.add(this.source.progressLoadingClass)
+  }
+  private uploadRequestFinished(): void {
+    this.progressBar.classList.remove(this.source.progressLoadingClass)
+    this.progressBar.classList.add(this.source.progressDoneClass)
+  }
+
+  private uploadRequestDidProgress(event): void {
+    const progress = (event.loaded / event.total) * 100
+    this.progressBar.value = progress
+  }
+
+  private createHiddenInput(): HTMLInputElement {
+    const hiddenField = document.createElement('input')
+    hiddenField.setAttribute("type", "hidden")
+    this.source.uploadFormTarget.appendChild(hiddenField)
+    return hiddenField
+  }
+
+  private removeElement(element: HTMLElement): void {
+    if (element && element.parentNode) {
+      element.parentNode.removeChild(element)
     }
   }
 }
