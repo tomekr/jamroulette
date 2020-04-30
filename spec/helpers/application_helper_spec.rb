3ae7a38ed9315eed601b028eac75c100a44d7f89
Tomek Rabczak <tomek.rabczak@gmail.com>
2020-04-30 15:34:35 -0500

    Use an aside link helper to select the currently displayed link in menu bar

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
@@ -32,6 +34,30 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     example 'user is not logged in' do
       expect(helper.has_unread_notifications?(nil)).to be false
+    end
+  end
+
+  describe '#aside_link_to' do
+    context 'link is to the current page' do
+      before { allow(helper).to receive(:current_page?).and_return true }
+
+      it 'adds is-active class to link' do
+        link = helper.aside_link_to('/current_page_page') do
+          'an icon for the link'
+        end
+        expect(link).to include('is-active')
+      end
+    end
+
+    context 'link is not to the current page' do
+      before { allow(helper).to receive(:current_page?).and_return false }
+
+      it 'does not add is-active class to link' do
+        link = helper.aside_link_to('/not_current_page') do
+          'an icon for the link'
+        end
+        expect(link).to_not include('is-active')
+      end
     end
   end
 end
